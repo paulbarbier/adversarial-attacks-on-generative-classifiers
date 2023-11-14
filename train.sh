c1275cabc73f606957c4e9110f18a4fb64840df8
Paul Barbier <paulb24@orange.fr>
2023-11-14 17:28:16 +0100

    chnage config name.

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 python -m train \
-    --config=configs/config-gfz.py \
+    --config=configs/fashion-mnist-gfz.py \
     --config.checkpoint_name="gfz-50-epochs-fashion-mnist" \
     --config.num_epochs=50

split train in train/test; fix metrics computation issue on whole test dataset; add code for multiple metrics; add metrics!

code/dataset_utils.py
@@ -5,6 +5,9 @@ from torch.utils import data
 from torchvision.datasets import FashionMNIST, MNIST
 import numpy as np
 
+from torch import Generator
+from torch.utils.data import random_split
+
 #https://jax.readthedocs.io/en/latest/notebooks/Neural_Network_and_Data_Loading.html#data-loading-with-pytorch
 
 def numpy_collate(batch):
@@ -33,36 +36,30 @@ class TransformImage:
         return np.array(image).reshape(28, 28, 1)/255.0
  
 
-def get_dataset(name: str):
+def get_dataset(name: str, train: bool):
     if name == "fashion-mnist":
-        fashion_mnist_train_ds = FashionMNIST(
+        fashion_mnist_ds = FashionMNIST(
             './data/', 
             download=True,
-            train=True,
+            train=train,
             transform=TransformImage(),
         )
-        fashion_mnist_test_ds = FashionMNIST(
-            './data/', 
-            download=True, 
-            train=False,
-        )
-        return fashion_mnist_train_ds, fashion_mnist_test_ds
+        return fashion_mnist_ds
     elif name == "mnist":
-        mnist_train_ds = MNIST(
+        mnist_ds = MNIST(
             './data/', 
             download=True,
-            train=True,
+            train=train,
             transform=TransformImage(),
         )
-        mnist_test_ds = MNIST(
-            './data/', 
-            download=True, 
-            train=False,
-        )
-        return mnist_train_ds, mnist_test_ds
+        return mnist_ds
     else:
       raise NotImplementedError
-    
+
+def split_dataset(ds, lengths, seed):
+    generator = Generator().manual_seed(seed)
+    splits = random_split(ds, lengths, generator=generator)
+    return splits
 
 def get_dataloader(dataset: data.Dataset, batch_size: int, shuffle: bool = True) -> data.DataLoader:
     dataloader = NumpyLoader(

code/evaluate.py
@@ -1,10 +1,10 @@
-import orbax.checkpoint as ocp
 from jax import random
 from ml_collections import ConfigDict
+from tqdm import tqdm
 
-from models.utils import sample_gaussian
-from dataset_utils import get_dataset
-from utils import get_classifier, get_data_config, get_dtype, load_checkpoint, prepare_test_dataset
+from dataset_utils import get_dataloader, get_dataset
+from utils import get_classifier, get_dtype, load_checkpoint
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, confusion_matrix
 from jax import random
 import jax.numpy as jnp
 from jax.nn import one_hot
@@ -17,7 +17,7 @@ from pathlib import Path
 def evaluate(flags):
   checkpoint = load_checkpoint(Path.cwd() / Path(flags.checkpoint))
 
-  config = ConfigDict(checkpoint["config"]) 
+  config = ConfigDict(checkpoint["config"])
 
   if flags.dtype == "":
     dtype = get_dtype(config.dtype)
@@ -30,35 +30,76 @@ def evaluate(flags):
 
   classifier = get_classifier(config) 
 
-  _, test_ds = get_dataset(config.dataset)
-  dataset_config = get_data_config(test_ds)
-  test_images, test_labels = prepare_test_dataset(
-    test_ds, dataset_config, dtype
-  )
+  eval_ds = get_dataset(config.dataset, train=False)
+  eval_dl = get_dataloader(eval_ds, config.test_batch_size, dtype)
 
-  model_config = classifier.create_model_config(config, dataset_config)
+  model_config = classifier.create_model_config(config)
   log_likelihood_fn = classifier.log_likelihood_A
+  loss_fn = classifier.loss_A
+  params = checkpoint["params"]
 
   eval_key = random.PRNGKey(config.eval_seed)
 
-  n_test = int(test_images.shape[0]*flags.ratio)
-  eval_key, test_accuracy_value = classifier.compute_batch_accuracy(
-    eval_key, 
-    model_config, 
-    checkpoint["params"], 
-    test_images[:n_test], 
-    test_labels[:n_test], 
-    log_likelihood_fn,
-    K=K,
-  )
-  print(f"\nAccuracy: {test_accuracy_value}% on {n_test} samples.")
+  #sink for the metric values
+  metrics = {
+    "loss": [],
+    "accuracy": [],
+    "f1_score_micro": [],
+    "f1_score_macro": [],
+    "precision_micro": [],
+    "precision_macro": [],
+    "recall_micro": [],
+    "recall_macro": [],
+    "confusion_matrix": [],
+  }
+
+  y_true, y_predictions, eval_losses = [], [], []
+  with tqdm(eval_dl, unit="batch") as teval:
+    teval.set_description(f"Evaluation")
+    for eval_step, (X_batch, y_batch) in enumerate(teval):
+      if flags.debug and eval_step == 2:
+        break
+      y_batch_one_hot = one_hot(y_batch, config.n_classes)
+
+      # compute batch prediction
+      eval_key, y_pred_batch = classifier.make_predictions(
+        eval_key, model_config, params, X_batch, log_likelihood_fn
+      )
+      y_predictions.append(y_pred_batch)
+      y_true.append(y_batch)
+
+      # compute batch loss
+      eval_key, batch_loss = classifier.compute_batch_loss(
+        eval_key, model_config, params, X_batch, y_batch_one_hot, loss_fn
+      )
+      eval_losses.append(batch_loss)
+
+  y_true = jnp.concatenate(y_true)
+  y_predictions = jnp.concatenate(y_predictions)
+  eval_losses = jnp.array(eval_losses)
+
+  metrics["accuracy"].append(accuracy_score(y_true, y_predictions))
+  metrics["precision_micro"].append(precision_score(y_true, y_predictions, average="micro"))
+  metrics["precision_macro"].append(precision_score(y_true, y_predictions, average="macro"))
+  metrics["f1_score_micro"].append(f1_score(y_true, y_predictions, average="micro"))
+  metrics["f1_score_macro"].append(f1_score(y_true, y_predictions, average="macro"))
+  metrics["recall_micro"].append(recall_score(y_true, y_predictions, average="micro"))
+  metrics["recall_macro"].append(recall_score(y_true, y_predictions, average="macro"))
+  metrics["confusion_matrix"].append(confusion_matrix(y_true, y_predictions))
+  metrics["loss"].append(jnp.mean(eval_losses))
+
+  metric_keys = ["loss", "accuracy", "precision_micro", "precision_macro", "recall_micro", "recall_macro", "f1_score_micro", "f1_score_macro"]
+  print("\n".join(f"{key}: {metrics[key][-1]:.3f}" for key in metric_keys))
+
+  print("Confusion matrix:")
+  print(metrics["confusion_matrix"][-1])
 
 
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string("checkpoint", "", "Checkpoint relative path.")
-flags.DEFINE_float("ratio", 1.0, "ratio of the test set to use for the evaluation")
 flags.DEFINE_string("dtype", "", "dtype")
+flags.DEFINE_bool("debug", False, "debug flag")
 flags.DEFINE_integer("K", -1, "number of samples to use for importance sampling")
 
 def main(argv):

code/train.py
@@ -3,17 +3,16 @@ from functools import partial
 from ml_collections import ConfigDict, config_flags
 
 from models.utils import sample_gaussian
-from dataset_utils import get_dataset, get_dataloader
+from dataset_utils import get_dataset, get_dataloader, split_dataset
 from utils import get_classifier, get_data_config, get_dtype, prepare_test_dataset
 from optimiser import get_optimiser
 from jax import random
+import jax.numpy as jnp
 from jax.nn import one_hot
 from flax.training import train_state
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, confusion_matrix
 from tqdm import tqdm
 import orbax.checkpoint as ocp
-
-import models.ClassifierGFZ as ClassifierGFZ
-import models.ClassifierDFZ as ClassifierDFZ
 
 from absl import app
 from absl import flags
@@ -21,27 +20,24 @@ from pathlib import Path
 
 CHECKPOINT_DIR = Path.cwd() / Path("checkpoints")
 
+def train_and_evaluate(flags):
+    config = flags.config
 
-def train_and_evaluate(config: ConfigDict):
     dtype = get_dtype(config.dtype)
 
-    checkpointer = None
     if config.checkpoint:
         checkpointer = ocp.PyTreeCheckpointer()
 
-    train_ds, test_ds = get_dataset(config.dataset)
-    train_dl = get_dataloader(train_ds, config.batch_size, dtype)
+    ds = get_dataset(config.dataset, train=True)
+    train_ds, test_ds = split_dataset(ds, [config.n_train, config.n_test], config.train_seed)
 
-    dataset_config = get_data_config(train_ds)
-
-    test_images, test_labels = prepare_test_dataset(
-       test_ds, dataset_config, dtype
-    )
+    train_dl = get_dataloader(train_ds, config.train_batch_size, dtype)
+    test_dl = get_dataloader(test_ds, config.test_batch_size, dtype)
     
     key = random.PRNGKey(config.train_seed)
 
     classifier = get_classifier(config)
-    model_config = classifier.create_model_config(config, dataset_config)
+    model_config = classifier.create_model_config(config)
     key, init_params = classifier.init_params(key, model_config, dtype)
 
     optimiser = get_optimiser(config)
@@ -52,24 +48,36 @@ def train_and_evaluate(config: ConfigDict):
     # split training/test keys
     key, training_key, dropout_key, test_key = random.split(key, 4)
 
-    #sinks for the metric values
-    training_steps, test_steps = [], []
-    training_loss_values, test_loss_values, test_accuracy_values = [], [], []
-    loss_value, test_loss_value, test_accuracy_value = None, None, None
-
-    epsilon_shape = (config.batch_size, config.model.d_latent)
-
+    #sink for the metric values
+    metrics = {
+        "training_steps": [],
+        "test_steps": [],
+        "training_loss": [],
+        "test_loss": [],
+        "test_accuracy": [],
+        "test_f1_score_micro": [],
+        "test_f1_score_macro": [],
+        "test_precision_micro": [],
+        "test_precision_macro": [],
+        "test_recall_micro": [],
+        "test_recall_macro": [],
+        "test_confusion_matrix": [],
+    }
+    
+    epsilon_shape = (config.train_batch_size, config.model.d_latent)
     log_likelihood_fn = classifier.log_likelihood_A
     loss_fn = classifier.loss_A
     loss_single_fn = classifier.loss_A_single
 
     # training loop
     for epoch in range(1, config.num_epochs+1):
-        with tqdm(train_dl, unit="batch") as tepoch:
-            tepoch.set_description(f"Epoch {epoch}")
-            for train_step, (X_batch, y_batch) in enumerate(tepoch):
+        with tqdm(train_dl, unit="batch") as train_epoch:
+            train_epoch.set_description(f"Train epoch #{epoch}")
+            for train_step, (X_batch, y_batch) in enumerate(train_epoch):
+                if flags.debug and train_step == 100:
+                    break
                 # one-hot encoding
-                y_batch_one_hot = one_hot(y_batch, dataset_config.n_classes)
+                y_batch_one_hot = one_hot(y_batch, config.n_classes)
                 
                 # sample the prior from gaussian distribution
                 training_key, epsilon = sample_gaussian(training_key, epsilon_shape)
@@ -80,45 +88,58 @@ def train_and_evaluate(config: ConfigDict):
                 )
                 
                 # log the training loss
-                training_loss_values.append(loss_value)
-                training_steps.append(epoch*config.num_epochs + train_step+1)
+                metrics["training_loss"].append(loss_value)
+                metrics["training_steps"].append(epoch*config.num_epochs + train_step+1)
 
-                tepoch.set_postfix(training_loss=loss_value, test_loss=test_loss_value, test_accuracy=test_accuracy_value)
+                train_epoch.set_postfix(training_loss=loss_value)
+            
+            y_true, y_predictions, test_losses = [], [], []
+            with tqdm(test_dl, unit="batch") as test_epoch:
+                test_epoch.set_description(f"Test epoch #{epoch}")
+                for test_step, (X_batch, y_batch) in enumerate(test_epoch):
+                    if flags.debug and test_step == 2:
+                        break
+                    y_batch_one_hot = one_hot(y_batch, config.n_classes)
 
-            # compute test loss and accuracy
-            test_key, test_loss_value = classifier.compute_batch_loss(
-                test_key, model_config, training_state.params, test_images, test_labels, loss_fn,
-            )
+                    # compute batch prediction
+                    test_key, y_pred_batch = classifier.make_predictions(
+                        test_key, model_config, training_state.params, X_batch, log_likelihood_fn
+                    )
+                    y_predictions.append(y_pred_batch)
+                    y_true.append(y_batch)
 
-            test_key, test_accuracy_value = classifier.compute_batch_accuracy(
-                test_key, 
-                model_config, 
-                training_state.params, 
-                test_images[:100], 
-                test_labels[:100], 
-                log_likelihood_fn,
-            )
+                    # compute batch loss
+                    test_key, batch_loss = classifier.compute_batch_loss(
+                        test_key, model_config, training_state.params, X_batch, y_batch_one_hot, loss_fn
+                    )
+                    test_losses.append(batch_loss)
 
-            test_loss_values.append(test_loss_value)
-            test_accuracy_values.append(test_accuracy_value)
-            test_steps.append(epoch*config.num_epochs + train_step+1)
+            y_true = jnp.concatenate(y_true)
+            y_predictions = jnp.concatenate(y_predictions)
+            test_losses = jnp.array(test_losses)
 
-            tepoch.set_postfix(
-               training_loss=loss_value, test_loss=test_loss_value, accuracy=test_accuracy_value
-            )
+            metrics["test_steps"].append((epoch-1)*config.num_epochs + train_step+1)
+            metrics["test_accuracy"].append(accuracy_score(y_true, y_predictions))
+            metrics["test_precision_micro"].append(precision_score(y_true, y_predictions, average="micro"))
+            metrics["test_precision_macro"].append(precision_score(y_true, y_predictions, average="macro"))
+            metrics["test_f1_score_micro"].append(f1_score(y_true, y_predictions, average="micro"))
+            metrics["test_f1_score_macro"].append(f1_score(y_true, y_predictions, average="macro"))
+            metrics["test_recall_micro"].append(recall_score(y_true, y_predictions, average="micro"))
+            metrics["test_recall_macro"].append(recall_score(y_true, y_predictions, average="macro"))
+            metrics["test_confusion_matrix"].append(confusion_matrix(y_true, y_predictions))
+            metrics["test_loss"].append(jnp.mean(test_losses))
+
+            metric_keys = ["test_loss", "test_accuracy", "test_precision_micro", "test_recall_micro", "test_f1_score_micro"]
+            print(", ".join(f"{key}: {metrics[key][-1]:.3f}" for key in metric_keys))
+
             if config.checkpoint:
                 checkpointer.save(
                     CHECKPOINT_DIR / f"{config.checkpoint_name}-{epoch}",
                     {
-                        "model": model_config,
-                        "params": training_state.params,
                         "config": config.to_dict(),
-                        "dataset_config": dataset_config.to_dict(),
-                        "training_steps": training_steps,
-                        "test_steps": test_steps,
-                        "training_loss_values": training_loss_values,
-                        "test_loss_values": test_loss_values,
-                        "test_accuracy_values": test_accuracy_values, 
+                        "model_config": model_config,
+                        "params": training_state.params,
+                        "metrics": metrics, 
                     }
                 )
 
@@ -131,8 +152,10 @@ config_flags.DEFINE_config_file(
     lock_config=True,
 )
 
+flags.DEFINE_bool("debug", False, "debug flag")
+
 def main(argv):
-  train_and_evaluate(FLAGS.config)
+  train_and_evaluate(FLAGS)
 
 if __name__ == '__main__':
   app.run(main)
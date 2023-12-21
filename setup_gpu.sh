128f3065c3b00fed656c339c01cf3ccb4dc11a01
Paul Barbier <paulb24@orange.fr>
2023-12-21 21:25:24 +0000

    fix setup gpu.

--- setup_gpu.sh
+++ setup_gpu.sh
@@ -4,5 +4,5 @@ sudo apt update && sudo apt install -y tmux
 
 cd code
 python3 -m pip install --upgrade pip
-python3 -m pip install -r tpu_requirements.txt
-python3 -m pip install --upgrade jax[cuda] -f https://storage.googleapis.com/jax-releases/jax_releases.html
+python3 -m pip install -r gpu_requirements.txt
+python3 -m pip install --upgrade jax[cuda] -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html

fix deepfool perturbation norm calculation.

code/attack.py
@@ -76,18 +76,16 @@ def attack(flags):
       )
 
       corrupted_indices = y_pred_batch != y_batch
-      X_corrupted_success = jnp.take(X_corrupted_batch, corrupted_indices, axis=0)
-      X_batch_success = jnp.take(X_batch, corrupted_indices, axis=0)
 
       metrics["pertubation_norms"].append(
-        jnp.mean(perturbation_norm(X_corrupted_success, X_batch_success))
+        jnp.mean(perturbation_norm(X_batch, X_corrupted_batch, corrupted_indices))
       )
       metrics["attack_success_rate"].append(
         jnp.mean(corrupted_indices)
       )
       tattack.set_postfix(
-        success_rate=np.mean(metrics["pertubation_norms"]),
-        perturbation_norm=np.mean(metrics["attack_success_rate"]),
+        pertubation_norm=np.mean(metrics["pertubation_norms"]),
+        success_rate=np.mean(metrics["attack_success_rate"]),
       )
   
   metrics["pertubation_norms"] = np.mean(metrics["pertubation_norms"])

code/attack_and_detect.py
@@ -105,15 +105,13 @@ def attack_and_detect(flags):
       )
       
       corrupted_indices = jnp.argmax(attacked_logits, axis=1) != y_batch
-      X_corrupted_success = jnp.take(X_corrupted_batch, corrupted_indices, axis=0)
-      X_batch_success = jnp.take(X_batch, corrupted_indices, axis=0)
-
       metrics["pertubation_norms"].append(
-        jnp.mean(perturbation_norm(X_corrupted_success, X_batch_success))
+        jnp.mean(perturbation_norm(X_batch, X_corrupted_batch))
       )
       metrics["attack_success_rate"].append(
         jnp.mean(corrupted_indices)
       )
+      
       tattack.set_postfix(
         detection_rate_at_5_pc=metrics["detection_rate_at_5_pc"][-1],
         attack_success_rate=np.mean(metrics["attack_success_rate"]),

code/utils.py
@@ -70,6 +70,9 @@ def get_attack_model(config: ConfigDict):
         raise NotImplementedError(config.attack_name)
     return attack_model
 
-@jax.vmap
-def perturbation_norm(truth, predicted):
-    return jnp.linalg.norm(truth - predicted) / jnp.linalg.norm(truth)
+@jax.jit
+def perturbation_norm(truth, perturbated, indices = None):
+    delta = jnp.sum((truth - perturbated)**2, axis=(1, 2, 3), where=indices)
+    perturbation = delta / jnp.sum(truth**2, axis=(1, 2, 3), where=indices)
+    perturbation = jnp.sqrt(perturbation)
+    return perturbation